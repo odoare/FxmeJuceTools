2677ef23007faadbff31b2af53a1afa8d5548a95
Olivier Doaré <olivier.doare@ensta-paris.fr>
2025-10-19 08:45:37 +0200

    Add getParameter function.

--- module/FxmeJuceTools/Components/FxmeButton.h
+++ module/FxmeJuceTools/Components/FxmeButton.h
@@ -11,6 +11,7 @@ public:
     FxmeButton(juce::AudioProcessorValueTreeState& apvts
                 , juce::String paramName = "dummy"
                 , juce::Colour colour = juce::Colours::white)
+        : apvtsRef(apvts), parameterID(paramName)
     {
         button.setColour(juce::ToggleButton::tickColourId,colour);
         button.setButtonText(paramName);
@@ -22,6 +23,7 @@ public:
                 , juce::String paramName = "dummy"
                 , juce::String labelText = "Button"
                 , juce::Colour colour = juce::Colours::white)
+        : apvtsRef(apvts), parameterID(paramName)
     {
         button.setColour(juce::ToggleButton::tickColourId,colour);
         button.setButtonText(labelText);
@@ -44,9 +46,16 @@ public:
         button.setLookAndFeel(newLookAndFeel);
     }
 
+    juce::RangedAudioParameter* getParameter() const
+    {
+        return apvtsRef.getParameter(parameterID);
+    }
+
     juce::ToggleButton button;
 
 private:
+    juce::AudioProcessorValueTreeState& apvtsRef;
+    juce::String parameterID;
 
     std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> attachment;
 
