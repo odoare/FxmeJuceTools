1df6540290e25dce777b8d0bf1c7a6d8700812ba
Olivier Doaré <olivier.doare@ensta-paris.fr>
2026-03-05 19:56:02 +0100

    New FxmeSlider component

---
+++ module/FxmeJuceTools/Components/FxmeSlider.h
@@ -0,0 +1,74 @@
+/*
+  ==============================================================================
+
+    FxmeSlider.h
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+class FxmeSlider : public juce::Slider
+{
+public:
+    FxmeSlider(juce::AudioProcessorValueTreeState& apvts, 
+               const juce::String& paramID, 
+               const juce::String& labelText, 
+               const juce::Colour& colour)
+    {
+      setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
+      setName(labelText);
+
+      attachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(apvts, paramID, *this);
+    }
+    ~FxmeSlider() override
+    {
+    }
+
+    void mouseDown(const juce::MouseEvent& e) override
+    {
+        if (e.mods.isPopupMenu())
+    {
+        auto* label = new juce::Label();
+        label->setEditable(true);
+        label->setText(getTextFromValue(getValue()), juce::NotificationType::dontSendNotification);
+        label->setJustificationType(juce::Justification::centred);
+        
+        label->setColour(juce::Label::backgroundColourId, juce::Colours::black);
+        label->setColour(juce::Label::textColourId, juce::Colours::white);
+        label->setColour(juce::Label::outlineColourId, juce::Colours::grey);
+        
+        auto bounds = getScreenBounds();
+        label->setBounds(0, 0, juce::jmin(80, bounds.getWidth()), 20);
+        label->setCentrePosition(bounds.getCentre());
+        
+        juce::Component::SafePointer<FxmeSlider> safeThis(this);
+        
+        label->onTextChange = [safeThis, label]() {
+            if (safeThis)
+                safeThis->setValue(safeThis->getValueFromText(label->getText()));
+        };
+        
+        label->onEditorHide = [label]() {
+            juce::MessageManager::getInstance()->callAsync([label]() { delete label; });
+        };
+        
+        label->addToDesktop(juce::ComponentPeer::windowIsTemporary |         juce::ComponentPeer::windowHasDropShadow);
+        label->setVisible(true);
+        label->showEditor();
+    }
+    else
+    {
+        juce::Slider::mouseDown(e);
+    }
+
+    }
+
+private:
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> attachment;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(FxmeSlider)    
+};
+
